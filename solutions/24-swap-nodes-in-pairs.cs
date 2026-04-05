b134f31d09b96ee309bc8c29d711419e3230baac
Devid Duma <devidd05@gmail.com>
2026-04-05 13:39:06 +0200

    Add 24-swap-nodes-in-pairs

---
+++ solutions/24-swap-nodes-in-pairs.cs
@@ -0,0 +1,72 @@
+public class SwapNodesInPairsSolution 
+{
+    public ListNode SwapPairs(ListNode head)
+    {
+        ListNode result = null;
+        ListNode resultLast = null;
+        
+        // 2-Group, reverse in pairs
+        const int k = 2;
+        
+        // Index of node in list
+        var index = -1;
+        
+        // Segment of reversed nodes in K-Group
+        ListNode segment = null;
+        ListNode segmentLast = null;
+        
+        // Rest nodes
+        ListNode restNodes = null;
+        var restNodesIndex = index;
+        
+        for (var currNode = head; currNode != null; currNode = currNode.next)
+        {
+            index += 1;
+            
+            // New node of segment
+            var newNode = new ListNode(currNode.val, segment);
+            segment = newNode;
+            
+            // First element of segment
+            if (index % k == 0)
+            {
+                segmentLast = newNode;
+                restNodes = currNode;
+                restNodesIndex = index;
+            }
+            // Last element of segment
+            else if (index % k == k - 1)
+            {
+                // Add segment to result
+                if (result == null)
+                {
+                    result = segment;
+                    resultLast = segmentLast;
+                }
+                else
+                {
+                    resultLast.next = segment;
+                    resultLast = segmentLast;
+                }
+                
+                // Reset segment
+                segment = null;
+            }
+        }
+        
+        // Add rest nodes in the end
+        if (index - restNodesIndex + 1 < k)
+        {
+            if (resultLast == null)
+            {
+                result = restNodes;
+            }
+            else
+            {
+                resultLast.next = restNodes;
+            }
+        }
+        
+        return result;
+    }
+}
